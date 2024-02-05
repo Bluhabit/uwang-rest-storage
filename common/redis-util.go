16282f4991d0a3a2b322fb42b8e2b4c6eeaa03b2
trian damai <triannurizkillah@gmail.com>
2024-02-05 17:59:02 +0700

    skip godotenv if not exist

--- common/redis-util.go
+++ common/redis-util.go
@@ -8,9 +8,11 @@ import (
 
 func CreateRedisKeyUserSession(SessionId string) string {
 	var err error
-	err = godotenv.Load()
-	if err != nil {
-		return ""
+	if len(os.Getenv("ENVIRONMENT")) < 1 {
+		err = godotenv.Load()
+		if err != nil {
+			return ""
+		}
 	}
 	environment := os.Getenv("ENVIRONMENT")
 	return fmt.Sprintf("%s:sign-in:%s", environment, SessionId)
